703ba152581d15a5450d4a0d60c5e8109d7e6dcd
Lovin Yarn <juanshengyuan@gmail.com>
2026-05-15 20:52:39 +0800

    luci: embed netdata dashboard directly

--- luci-app-netdata/htdocs/luci-static/resources/view/netdata.js
+++ luci-app-netdata/htdocs/luci-static/resources/view/netdata.js
@@ -5,30 +5,12 @@
 
 return view.extend({
 	render() {
-		const origin = 'http://' + window.location.hostname + ':19999';
-
 		return E('div', {'class': 'cbi-map' }, [
 			E('h2', {'name': 'content'}, [
 				_('Netdata Dashboard')
 			]),
-			E('div', {
-				'style': 'display: flex; gap: 8px; align-items: center; margin: 0 0 12px 0;'
-			}, [
-				E('a', {
-					'class': 'btn cbi-button cbi-button-action',
-					'href': origin + '/v3/',
-					'target': '_blank',
-					'rel': 'noopener noreferrer'
-				}, _('Open V3 Dashboard')),
-				E('a', {
-					'class': 'btn cbi-button',
-					'href': origin + '/',
-					'target': '_blank',
-					'rel': 'noopener noreferrer'
-				}, _('Open Dashboard'))
-			]),
 			E('iframe', {
-				'src': origin + '/v3/',
+				'src': 'http://' + window.location.hostname + ':19999',
 				'style': 'width: 100%; min-height: 1200px; border: none; border-radius: 3px; resize: vertical;'
 			}, null)
 		]);
