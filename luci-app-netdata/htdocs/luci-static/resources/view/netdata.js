cd92ad3e1493a5e2b2fca7b08a2c69381fbc56d5
Lovin Yarn <juanshengyuan@gmail.com>
2026-05-15 19:36:25 +0800

    luci: default netdata dashboard to v1

--- luci-app-netdata/htdocs/luci-static/resources/view/netdata.js
+++ luci-app-netdata/htdocs/luci-static/resources/view/netdata.js
@@ -5,12 +5,30 @@
 
 return view.extend({
 	render() {
+		const origin = 'http://' + window.location.hostname + ':19999';
+
 		return E('div', {'class': 'cbi-map' }, [
 			E('h2', {'name': 'content'}, [
 				_('Netdata Dashboard')
 			]),
+			E('div', {
+				'style': 'display: flex; gap: 8px; align-items: center; margin: 0 0 12px 0;'
+			}, [
+				E('a', {
+					'class': 'btn cbi-button cbi-button-action',
+					'href': origin + '/v3/',
+					'target': '_blank',
+					'rel': 'noopener noreferrer'
+				}, _('Open V3 Dashboard')),
+				E('a', {
+					'class': 'btn cbi-button',
+					'href': origin + '/v1/',
+					'target': '_blank',
+					'rel': 'noopener noreferrer'
+				}, _('Open V1 Dashboard'))
+			]),
 			E('iframe', {
-				'src': 'http://' + window.location.hostname + ':19999',
+				'src': origin + '/v1/',
 				'style': 'width: 100%; min-height: 1200px; border: none; border-radius: 3px; resize: vertical;'
 			}, null)
 		]);
